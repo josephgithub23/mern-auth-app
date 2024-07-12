1f3de041aeace9adee9bfa347ffa82bae63e127a
Joseph Tobi Olatilo <125037114+josephgithub23@users.noreply.github.com>
2024-07-12 15:32:58 +0100

    add redux persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,25 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import useReducer from "./user/user.slice";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+// To Combine all the reducers
+const rootReducer = combineReducers({ user: useReducer });
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: { user: useReducer },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
 });
+
+export const persistor = persistStore(store);
