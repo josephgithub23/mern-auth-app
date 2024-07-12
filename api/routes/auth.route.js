9aaa40f70a3d74228d7836e0d9e2726bcfa5a482
Joseph Tobi Olatilo <125037114+josephgithub23@users.noreply.github.com>
2024-07-12 09:57:30 +0100

    create sign in api route

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import signup from "../controller/auth.Controller.js";
+import signup, { signin } from "../controller/auth.Controller.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
+router.post("/signin", signin);
 
 export default router;
